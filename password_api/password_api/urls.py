874219f76ad5a35fd14edb092c9927644ce1929a
MasterGowen <___Cantd3stroY>
2018-11-05 17:48:05 +0500

    initial#14

--- password_api/password_api/urls.py
+++ password_api/password_api/urls.py
@@ -5,12 +5,12 @@ from rest_framework import routers
 from core import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'companies', views.CompanyViewSet)
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'password_cards', views.PasswordCardViewSet)
-router.register(r'user_registrations', views.UserRegistrationViewSet)
+router.register(r'user', views.UserViewSet)
+router.register(r'group', views.GroupViewSet)
+router.register(r'company', views.CompanyViewSet)
+router.register(r'category', views.CategoryViewSet)
+router.register(r'card', views.PasswordCardViewSet)
+router.register(r'user_registration', views.UserRegistrationViewSet)
 
 urlpatterns = [
     path('admin', admin.site.urls),
