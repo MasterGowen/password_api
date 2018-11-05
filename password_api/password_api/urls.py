0c0c420da1b5b5bbee85ad2d35078e8948c1e660
MasterGowen <___Cantd3stroY>
2018-11-05 17:32:04 +0500

    initial#6

--- password_api/password_api/urls.py
+++ password_api/password_api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from ..core import views
+from core import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
